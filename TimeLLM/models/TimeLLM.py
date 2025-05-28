dedf4e401795807457bf3d8afe2810f3338f8242
Nathan <lwzheng777@outlook.com>
2025-05-28 15:19:36 +0930

    update TimeLLM

--- TimeLLM/models/TimeLLM.py
+++ TimeLLM/models/TimeLLM.py
@@ -88,6 +88,56 @@ class NoLLM_Backbone(nn.Module):
 
 
 
+
+
+class TimeTextMixer(nn.Module):
+    def __init__(self, seq_len, text_topk, compress_token, d_model=None):
+        super().__init__()
+        self.text_topk = text_topk 
+        self.seq_len = seq_len
+        self.d_model = d_model
+        self.compress_token = compress_token
+        
+        self.mlp_1 = nn.Sequential(nn.Linear(self.seq_len, self.compress_token),
+                                   nn.ReLU(),
+                                   nn.LayerNorm(self.compress_token),
+                                   nn.Linear(self.compress_token, self.compress_token),
+                                   nn.ReLU(),
+                                   nn.LayerNorm(self.compress_token))
+
+
+        self.mlp_2 = nn.Sequential(nn.Linear(self.d_model, self.d_model),
+                                    nn.ReLU(),
+                                    nn.LayerNorm(self.d_model),
+                                    nn.Linear(self.d_model, self.d_model),
+                                    nn.ReLU(),
+                                    nn.LayerNorm(self.d_model))
+        
+        # self.words2proto = nn.Linear(self.words_embedding.shape[0], 1000)
+        
+    
+    def forward(self, x):
+        # B = x.shape[0]
+        # words = self.words2proto(self.words_embedding.permute(1, 0))
+        # words = words.permute(1, 0).unsqueeze(0)
+        # _, attn_weights = self.att(x, words.expand(x.shape[0], -1, -1), words.expand(x.shape[0], -1, -1))
+        # topk_weight, topk_indices = torch.topk(attn_weights, k=self.text_topk, dim=-1)
+        # words_expand = words.unsqueeze(0).expand(x.shape[0], x.shape[1], -1, -1) #[32, 1000, 768]
+        
+        # topk_v = torch.gather(words_expand, dim=2, index=topk_indices.unsqueeze(-1).expand(-1, -1, -1, 768))  # (32, 4, 64, topk, 768)
+        # topk_v = topk_v.view(B, -1, 768)
+        # x = torch.concat([topk_v, x], dim=1)
+        # outputs = topk_text + outputs
+        x = x.permute(0, 2, 1)
+        x = self.mlp_1(x)
+        
+        x = x.permute(0, 2, 1)
+        x = self.mlp_2(x)
+        return x 
+
+
+
+
 class Model(nn.Module):
 
     def __init__(self, configs, patch_len=16, stride=8):
@@ -126,22 +176,26 @@ class Model(nn.Module):
             self.word_embeddings = self.llama.get_input_embeddings().weight
             self.word_encoder = self.llama.get_input_embeddings()
             del self.llama
+            
         else:
             self.word_embeddings = self.llm.get_input_embeddings().weight
             
-            
         self.vocab_size = self.word_embeddings.shape[0]
+
         self.num_tokens = 1000
         self.mapping_layer = nn.Linear(self.vocab_size, self.num_tokens)
+
         self.reprogramming_layer = ReprogrammingLayer(configs.d_model, configs.n_heads, self.d_ff, self.d_llm)
+
         self.patch_nums = int((configs.seq_len - self.patch_len) / self.stride + 2)
+        # self.head_nf = self.d_ff * self.patch_nums
         self.head_nf = self.d_ff * self.patch_nums
 
         if self.task_name == 'long_term_forecast' or self.task_name == 'short_term_forecast':
             self.output_projection = FlattenHead(configs.enc_in, self.head_nf, self.pred_len, head_dropout=configs.dropout)
             
         elif configs.task_name == 'classification':
-            self.output_projection = nn.Linear(in_features=self.d_llm, out_features=self.configs.n_classes)
+            self.output_projection = nn.Linear(in_features=self.d_llm, out_features=self.n_classes)
             
         elif configs.task_name == 'imputation':
             self.output_projection = FlattenHead(configs.enc_in, self.head_nf, configs.seq_len, head_dropout=configs.dropout)
@@ -149,25 +203,40 @@ class Model(nn.Module):
         elif configs.task_name == 'anomaly_detection':
             self.output_projection = FlattenHead(configs.enc_in, self.head_nf, configs.seq_len, head_dropout=configs.dropout)
             
+            
         self.normalize_layers = Normalize(configs.enc_in, affine=False)
+        
+        self.mixer = TimeTextMixer(seq_len=212, text_topk=5, compress_token=64, d_model=self.d_llm)
         
         
     def forward(self, x_enc, x_mark_enc, x_dec, x_mark_dec, prompt, mask=None):
         if self.task_name == 'long_term_forecast' or self.task_name == 'short_term_forecast':
-            dec_out = self.forecast(x_enc, None, None, None, prompt=prompt)
-            return dec_out[:, -self.pred_len:, :]
-        
+            dec_out, enc_out, ts_af = self.forecast(x_enc, None, None, None, prompt=prompt)
+            return dec_out[:, -self.pred_len:, :], enc_out, ts_af
         elif self.task_name == 'imputation':
             dec_out = self.imputation(x_enc, None, None, None, prompt)
             return dec_out
-        
         elif self.task_name == 'anomaly_detection':
             dec_out = self.anomaly_detection(x_enc, prompt)
             return dec_out
+
+
+
+    def pad_or_truncate_batch(self, embeddings, target_len=200):
+        """
+        embeddings: torch.Tensor of shape (B, L, D)
+        Returns: tensor of shape (B, target_len, D)
+        """
+        B, L, D = embeddings.shape
+
+        if L < target_len:
+            pad_len = target_len - L
+            padding = torch.zeros(B, pad_len, D, device=embeddings.device, dtype=embeddings.dtype)
+            embeddings = torch.cat([embeddings, padding], dim=1)  # pad at end
+        elif L > target_len:
+            embeddings = embeddings[:, :target_len, :]
         
-        elif self.task_name == 'classification':
-            dec_out = self.classification(x_enc, prompt)
-            return dec_out
+        return embeddings
 
 
     def forecast(self, x_enc, x_mark_enc, x_dec, x_mark_dec, prompt):
@@ -187,9 +256,18 @@ class Model(nn.Module):
         x_enc = x_enc.permute(0, 2, 1).contiguous()
         enc_out, n_vars = self.patch_embedding(x_enc)
         enc_out = self.reprogramming_layer(enc_out, source_embeddings, source_embeddings)
+        ts_len = enc_out.shape[1]
+        prompt_embeddings = self.pad_or_truncate_batch(prompt_embeddings)
         llama_enc_out = torch.cat([prompt_embeddings, enc_out], dim=1)
+        
+        llama_enc_out = self.mixer(llama_enc_out) # comment the mixer if you dont need it
+        
         dec_out = self.llm(inputs_embeds=llama_enc_out).last_hidden_state
         
+        ts_af = dec_out[:, ts_len:, :]
+        ts_af = ts_af[:, :165, :]  # used to take the ts after llm token
+        enc_out = enc_out[:, :165, :] # used to take ts before llm token
+
         dec_out = dec_out[:, :, :self.d_ff]
         dec_out = torch.reshape(dec_out, (-1, n_vars, dec_out.shape[-2], dec_out.shape[-1]))
         dec_out = dec_out.permute(0, 1, 3, 2).contiguous()
@@ -197,7 +275,7 @@ class Model(nn.Module):
         dec_out = dec_out.permute(0, 2, 1).contiguous()
         dec_out = self.normalize_layers(dec_out, 'denorm')
         
-        return dec_out
+        return dec_out, enc_out, ts_af
 
 
     def imputation(self, x_enc, x_mark_enc, x_dec, x_mark, prompt):
